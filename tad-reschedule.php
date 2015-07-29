13f3e5e2f49fe7c386a10292934d3a8d3af89003
Luca Tumedei <luca@theaveragedev.com>
2015-07-29 18:59:30 +0200

    added destruct test

--- tad-reschedule.php
+++ tad-reschedule.php
@@ -23,7 +23,17 @@ if ( ! function_exists( 'tad_reschedule' ) ) {
 
 class tad_Reschedule {
 
+	/**
+	 * @var string|callable A function name or a callable the result of which will be used to assert the until
+	 *      condition.
+	 */
+	public $until_callback;
+
 	public function until( $condition ) {
+		if ( is_callable( $condition ) ) {
+			$this->until_callback = $condition;
+		}
+
 		return $this;
 	}
 
@@ -41,5 +51,29 @@ class tad_Reschedule {
 		}
 
 		return $this;
+	}
+
+	/**
+	 * PHP 5 introduces a destructor concept similar to that of other object-oriented languages, such as C++.
+	 * The destructor method will be called as soon as all references to a particular object are removed or
+	 * when the object is explicitly destroyed or in any order in shutdown sequence.
+	 *
+	 * Like constructors, parent destructors will not be called implicitly by the engine.
+	 * In order to run a parent destructor, one would have to explicitly call parent::__destruct() in the destructor
+	 * body.
+	 *
+	 * Note: Destructors called during the script shutdown have HTTP headers already sent.
+	 * The working directory in the script shutdown phase can be different with some SAPIs (e.g. Apache).
+	 *
+	 * Note: Attempting to throw an exception from a destructor (called in the time of script termination) causes a
+	 * fatal error.
+	 *
+	 * @return void
+	 * @link http://php.net/manual/en/language.oop5.decon.php
+	 */
+	function __destruct() {
+		if ( $this->until_callback ) {
+			call_user_func( $this->until_callback );
+		}
 	}
 }
